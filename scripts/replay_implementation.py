88f1bbb97be21d2022f49de830549515276f3f41
Andrew White <andrewh@cdw.com>
2026-07-27 21:06:24 -0500

    fix: argparse type=bool cannot parse False from command line

--- scripts/replay_implementation.py
+++ scripts/replay_implementation.py
@@ -45,11 +45,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_path", type=str)
     parser.add_argument("--output_path", type=str)
-    parser.add_argument("--rgb_enabled", type=bool, default=True)
-    parser.add_argument("--segmentation_enabled", type=bool, default=True)
-    parser.add_argument("--depth_enabled", type=bool, default=True)
-    parser.add_argument("--instance_id_segmentation_enabled", type=bool, default=True)
-    parser.add_argument("--normals_enabled", type=bool, default=False)
+    def _str_to_bool(value):
+        if isinstance(value, bool):
+            return value
+        if value.lower() in ("yes", "true", "t", "y", "1"):
+            return True
+        if value.lower() in ("no", "false", "f", "n", "0"):
+            return False
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
+    parser.add_argument("--rgb_enabled", type=_str_to_bool, default=True)
+    parser.add_argument("--segmentation_enabled", type=_str_to_bool, default=True)
+    parser.add_argument("--depth_enabled", type=_str_to_bool, default=True)
+    parser.add_argument("--instance_id_segmentation_enabled", type=_str_to_bool, default=True)
+    parser.add_argument("--normals_enabled", type=_str_to_bool, default=False)
     parser.add_argument("--render_rt_subframes", type=int, default=1)
     parser.add_argument("--render_interval", type=int, default=1)
 
